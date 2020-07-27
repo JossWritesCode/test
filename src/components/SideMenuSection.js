a78d2db8b623ebede45b1ecf32dde9c75dd63290
JossWritesCode <joscelynrose@gmail.com>
2020-07-27 19:49:02 -0400

    submenu items are displaying

--- src/components/SideMenuSection.js
+++ src/components/SideMenuSection.js
@@ -1,12 +1,22 @@
-import React from "react";
-import sideMenuData from "../assets/accordion.json";
-import Section from "./SideMenuSection.js";
+import React, { useState } from "react";
+
+import SideMenuSubItem from "./SideMenuSubItem";
 
 function SideMenuSection({ sectionData, sectionName, logoLink }) {
+  const [toggle, setToggle] = useState(false);
   return (
     <section className="section">
-      <img alt={sectionName} src={logoLink} />
-      <h2 className="title">{sectionName}</h2>
+      <div onClick={() => setToggle(!toggle)} className="section-header">
+        <img alt={sectionName} src={logoLink} />
+        <h2 className="title">{sectionName}</h2>
+      </div>
+      {toggle ? (
+        <div className="accordion-section">
+          {sectionData.map((info, index) => (
+            <SideMenuSubItem subItemInfo={info} />
+          ))}
+        </div>
+      ) : null}
     </section>
   );
 }
--- src/components/SideMenuSubItem.js
+++ src/components/SideMenuSubItem.js
@@ -1,9 +1,30 @@
 import React from "react";
-import sideMenuData from "../assets/accordion.json";
-import Section from "./SideMenuSection.js";
+import XIcon from "../assets/icons/icon-x.svg";
+// content, date, employee, source, name
 
-function SideMenuSubItem({}) {
-  return <div className="sub-menu-item"></div>;
+function SideMenuSubItem({ subItemInfo }) {
+  return (
+    <div className="sub-menu-item">
+      <div className="sub-menu-item-list">
+        <p className="sub-menu-item-name">
+          {subItemInfo.name ? subItemInfo.name : null}
+        </p>
+        <p className="sub-menu-item-content">
+          {subItemInfo.content ? subItemInfo.content : null}
+        </p>
+        <p className="sub-menu-item-employee">
+          {subItemInfo.employee ? subItemInfo.employee : null}
+        </p>
+        <p className="sub-menu-item-source">
+          {subItemInfo.source ? subItemInfo.source : null}
+        </p>
+        <p className="sub-menu-item-date">
+          {subItemInfo.date ? subItemInfo.date : null}
+        </p>
+      </div>
+      <img src={XIcon} alt="close" />
+    </div>
+  );
 }
 
 export default SideMenuSubItem;
