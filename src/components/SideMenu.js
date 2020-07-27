6e72396f6daebf9f7e315036d94526231e53ae72
JossWritesCode <joscelynrose@gmail.com>
2020-07-27 17:55:37 -0400

    imported json

---
+++ src/components/SideMenu.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SideMenuData from "../assets/accordion.json";
+function SideMenu() {
+  console.log(SideMenuData, "sideMenuData");
+  return <div className="side-menu"></div>;
+}
+
+export default SideMenu;
