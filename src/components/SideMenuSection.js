6c44f56cf92aacaa07d439d269bdf5a408b53502
JossWritesCode <joscelynrose@gmail.com>
2020-07-27 20:23:52 -0400

    added number

--- src/components/SideMenuSection.js
+++ src/components/SideMenuSection.js
@@ -4,11 +4,13 @@ import SideMenuSubItem from "./SideMenuSubItem";
 
 function SideMenuSection({ sectionData, sectionName, logoLink }) {
   const [toggle, setToggle] = useState(false);
+  const [numberOfItems, setNumberOfItems] = useState(sectionData.length);
   return (
     <section className="section">
       <div onClick={() => setToggle(!toggle)} className="section-header">
         <img alt={sectionName} src={logoLink} />
         <h2 className="title">{sectionName}</h2>
+        <div className="item-number">{numberOfItems}</div>
       </div>
       {toggle ? (
         <div className="accordion-section">
